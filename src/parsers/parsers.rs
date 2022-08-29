b5bf076f830be057acf0622dda0497457bb5bf9e
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-08-29 13:58:48 -0400

    fixed static_lazy

--- src/parsers/parsers.rs
+++ src/parsers/parsers.rs
@@ -10,14 +10,16 @@ struct Scripture {
     verse: u8,
 }
 
-fn find(line: &str) -> Option<Vec<Scripture>> {
-
     // lazy_static insures that the regex is compiled only once.
     lazy_static!{
         // For testing the regex expression: https://regex101.com/r/fS3wA0/1
         static ref RE: Regex = Regex::new(r"(?:[1234]\s?)?[a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?").expect("Something went wrong during use of regex");
     }
-    todo!()
+
+fn regex_compilation(line: &str) -> Option<Vec<Scripture>> {
+
+todo!()
+
 }
 
 #[cfg(test)]
