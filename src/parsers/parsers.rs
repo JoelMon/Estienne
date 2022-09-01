47d8781cbd3ab79c1b4aa09bd5e05f278c79a3c2
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-09-01 19:25:27 -0400

    Fixed a few merge issues

--- src/parsers/parsers.rs
+++ src/parsers/parsers.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 use crate::locales::en_us::book;
+use regex::Regex;
+use lazy_static::lazy_static;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Scripture {
@@ -9,7 +11,36 @@ struct Scripture {
 }
 
 fn find(line: &str) -> Option<Vec<Scripture>> {
-    todo!()
+    todo!
+}
+
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Scripture {
+    name: String,
+    chapter: u8,
+    verse: u8,
+}
+
+    // lazy_static insures that the regex is compiled only once.
+    lazy_static!{
+        // For testing the regex expression: https://regex101.com/r/fS3wA0/1
+        static ref RE: Regex = Regex::new(r"\((?:[1234]\s?)?[a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)\ig").expect("Something went wrong during use of regex");
+
+        //static ref RE: Regex = Regex::new("[0-9]{3}-[0-9]{3}-[0-9]{4}").expect("Something went wrong");
+
+    }
+
+fn regex_compilation(line: &str) -> regex::Match {
+
+    let c_regex = &RE;
+
+    let found: regex::Match = c_regex.find(line).unwrap();
+
+    //println!("{}", &found.start());
+    //dbg!(&found.end());
+    found
+
 }
 
 #[cfg(test)]
@@ -18,6 +49,10 @@ mod test {
 
     #[test]
     fn t_find() {
-        assert!(true);
+       let thing = regex_compilation("phone: 111-222-3333");
+       dbg!(&thing);
+    println!("THING: {}", &thing.start());
+
+    assert_eq!((thing.start(), thing.end()), (7, 19));
     }
 }
