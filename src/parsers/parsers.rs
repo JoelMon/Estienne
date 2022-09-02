9143fb836df36e83d79e64d4d5eb462e2ec778ce
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-09-01 21:08:45 -0400

    made changes

--- src/parsers/parsers.rs
+++ src/parsers/parsers.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 use crate::locales::en_us::book;
-use regex::Regex;
 use lazy_static::lazy_static;
+use regex::Regex;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Scripture {
@@ -10,37 +10,10 @@ struct Scripture {
     verse: u8,
 }
 
-fn find(line: &str) -> Option<Vec<Scripture>> {
-    todo!
-}
-
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Scripture {
-    name: String,
-    chapter: u8,
-    verse: u8,
-}
-
-    // lazy_static insures that the regex is compiled only once.
-    lazy_static!{
-        // For testing the regex expression: https://regex101.com/r/fS3wA0/1
-        static ref RE: Regex = Regex::new(r"\((?:[1234]\s?)?[a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)\ig").expect("Something went wrong during use of regex");
-
-        //static ref RE: Regex = Regex::new("[0-9]{3}-[0-9]{3}-[0-9]{4}").expect("Something went wrong");
-
-    }
-
-fn regex_compilation(line: &str) -> regex::Match {
-
-    let c_regex = &RE;
-
-    let found: regex::Match = c_regex.find(line).unwrap();
-
-    //println!("{}", &found.start());
-    //dbg!(&found.end());
-    found
-
+// lazy_static insures that the regex is compiled only once.
+lazy_static! {
+    // For testing the regex expression: https://regex101.com/r/fS3wA0/1
+   static ref RE: Regex = Regex::new(r"/((?:[1234]\s?)?[a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)/ig").expect("Something went wrong during use of regex");
 }
 
 #[cfg(test)]
@@ -49,10 +22,9 @@ mod test {
 
     #[test]
     fn t_find() {
-       let thing = regex_compilation("phone: 111-222-3333");
-       dbg!(&thing);
-    println!("THING: {}", &thing.start());
-
-    assert_eq!((thing.start(), thing.end()), (7, 19));
+        let result = RE.find("Matthew 24:14").unwrap();
+        dbg!(result);
+        assert_eq!(result.start(), 0);
+        assert_eq!(result.end(), 5);
     }
 }
