fe8cb415153767f1b6734a7906a804456a41e97c
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-09-16 11:42:56 -0400

    Continued work on parsers

--- src/parsers/parsers.rs
+++ src/parsers/parsers.rs
@@ -1,7 +1,11 @@
-#![allow(dead_code)]
-use crate::locales::en_us::book;
+// #![allow(dead_code, unused_variables)]
+
 use lazy_static::lazy_static;
 use regex::Regex;
+
+type ScriptSlice = (Start, End);
+type Start = usize;
+type End = usize;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Scripture {
@@ -12,19 +16,35 @@ struct Scripture {
 
 // lazy_static insures that the regex is compiled only once.
 lazy_static! {
-    // For testing the regex expression: https://regex101.com/r/fS3wA0/1
-   static ref RE: Regex = Regex::new(r"/((?:[1234]\s?)?[a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)/ig").expect("Something went wrong during use of regex");
+    static ref RE: regex::Regex = Regex::new(r"/(?:[1234]\s?)?([a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\s*\d+[—–-]\d+|,\s*\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)/ig").expect("error while running regex");
+}
+
+pub fn find_scriptures(line: &str) -> Vec<ScriptSlice> {
+    let mut scrip_slices: Vec<ScriptSlice> = Vec::new();
+
+    let re: &regex::Regex = &RE;
+
+    dbg!("About to run");
+
+    for script in re.find_iter(&line) {
+        dbg!("Running... ");
+        dbg!(&script.start());
+
+        scrip_slices.push((script.start(), script.end()));
+    }
+
+    scrip_slices
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    // #[test]
-    // fn t_find() {
-    //     let result = RE.find("Matthew 24:14").unwrap();
-    //     dbg!(result);
-    //     assert_eq!(result.start(), 0);
-    //     assert_eq!(result.end(), 5);
-    // }
+    #[test]
+    fn t_find_scriptures() {
+        let line = r"John 3:16 and Matthew 24:14";
+        let result: Vec<ScriptSlice> = find_scriptures(line);
+
+        assert_eq!(result, vec![(0, 9), (14, 27)])
+    }
 }
