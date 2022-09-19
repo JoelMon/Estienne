4e9cf80763e143040a554d0f01070f714f3b13b8
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-09-19 14:17:43 -0400

    Fixed issue with iterator

--- src/parsers/parsers.rs
+++ src/parsers/parsers.rs
@@ -16,17 +16,15 @@ struct Scripture {
 
 // lazy_static insures that the regex is compiled only once.
 lazy_static! {
-    static ref RE: regex::Regex = Regex::new(r"/(?:[1234]\s?)?([a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\s*\d+[—–-]\d+|,\s*\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)/ig").expect("error while running regex");
+    static ref RE: regex::Regex = Regex::new(r"(?:[1234]\s?)?([a-zA-Z]+)(\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\s*\d+[—–-]\d+|,\s*\d+)*(?:;\s?\d+(?::(?:\d+[—–-]\d+|\d+)(?:,\d+[—–-]\d+|,\d+)*|;))*)?)").expect("error while running regex");
 }
 
 pub fn find_scriptures(line: &str) -> Vec<ScriptSlice> {
     let mut scrip_slices: Vec<ScriptSlice> = Vec::new();
-
     let re: &regex::Regex = &RE;
 
     dbg!("About to run");
-
-    for script in re.find_iter(&line) {
+    for script in re.find_iter(line) {
         dbg!("Running... ");
         dbg!(&script.start());
 
@@ -36,13 +34,14 @@ pub fn find_scriptures(line: &str) -> Vec<ScriptSlice> {
     scrip_slices
 }
 
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn t_find_scriptures() {
-        let line = r"John 3:16 and Matthew 24:14";
+        let line = "John 3:16 and Matthew 24:14";
         let result: Vec<ScriptSlice> = find_scriptures(line);
 
         assert_eq!(result, vec![(0, 9), (14, 27)])
