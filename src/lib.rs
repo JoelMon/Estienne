5a7df13f94d4bdff7f00733a6720a42c8731e1c0
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-09-21 13:13:45 -0400

    Started work

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,22 @@
 mod locales;
 mod parsers;
+
+pub fn surround(text: String, prefix: &str, postfix: &str) -> String {
+    todo!()
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn t_add_element_prefix_single() {
+        let text = "Another popular scripture is John 3:16, it's quoted often.".to_string();
+        let result = surround(text, "**", "**");
+        assert_eq!(
+            result,
+            String::from(
+                "Another popular scripture is this is a prefix **John 3:16** it's quoted often."
+            )
+        )
+    }
+}
